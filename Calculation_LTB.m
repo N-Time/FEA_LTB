1c003fb9a69b1908b6bdcf08bf9866ff4ff5e104
N-Time <51726922+N-Time@users.noreply.github.com>
2019-09-05 12:44:51 +0800

    Estimating the cases of multi-loads
    Add the function of estimating the cases of multi-loads

--- Calculation_LTB.m
+++ Calculation_LTB.m
@@ -1,14 +1,14 @@
 %%%%%%%%%%%%% Input %%%%%%%%%%%%%%
 %%% Origin Input
 % Material(N/m^2): E v
-E = 0.694 * 10^11;
+E = 2.1 * 10^11;
 vv = 0.3;
 % I-Section(mm): h hw bT tT bB tB
-h = 75.7; tw = 2.2;
-bT = 31.4; tT = 3.1;
-bB = 31.4; tB = 3.1;
+h = 300; tw = 7.1;
+bT = 150; tT = 10.7;
+bB = 150; tB = 10.7;
 % Length(m): L
-L = 1.9;
+L = 3;
 % Elements numbers: Nel
 Nel = 100;
 % In-plane BCs: v(z) v1(z)
@@ -21,11 +21,11 @@ phiz0 = [0]';
 phiz1 = [0]'; 
 % Loads(kN m)[Amplitude(kN m) Distribution(m) ay(mm){up<0, down>0}]: qy Py Mx0
 %%%% [Amp  ay] %%%%
-qy = [];
+qy = [0 0];
 %%%% [Amp  z  ay] %%%%
-Py = [1 0.85 -91];
+Py = [1 3 -150;1 3 150];
 %%%% [Amp  z] %%%%
-Mx0 = [];   
+Mx0 = [0 0];   
 
 %%%%%%%%%%%%% Output %%%%%%%%%%%%%%
 Mcr = FEA_LTB(E,vv,h,tw,bT,tT,bB,tB,L,Nel,vz0,vz1,uz0,uz1,phiz0,phiz1,qy,Py,Mx0) % kN m
